4a6abf29e87bff191b0acae803c3acba79017660
ToxicBlades <your.email@example.com>
2026-08-24 12:21:05 +0300

    fix: ts remove base url

--- libs/email/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { Resend } from "resend";
-import { envEmail } from "./env-config";
-
-//TODO: change resend into a separate service which handles emailing via user credentials
-export const resend = new Resend(envEmail.RESEND_TOKEN);
